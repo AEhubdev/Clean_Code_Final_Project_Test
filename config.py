bc8b540df8a40e0f6fd814df33407d7c53ce44e1
AEhubdev <aliaksei.entsin@gmail.com>
2025-12-17 23:45:40 +0100

    testing the clean code

--- config.py
+++ config.py
@@ -8,5 +8,3 @@ STOCH_D_PERIOD = 3
 TICKER = "GC=F"
 TIMEFRAME_OPTIONS = {"1 Day": "1d", "1 Hour": "1h", "15 Min": "15m"}
 DEFAULT_INTERVAL = "1mo"
-# Map display names to yfinance intervals
-TIMEFRAME_OPTIONS = {"1 Day": "1d", "1 Hour": "1h", "15 Min": "15m"}
